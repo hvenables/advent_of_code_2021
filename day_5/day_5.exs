5a035e09857ec27b99b7292b18cdc4b0669b1ea0
Harry Venables <harry.venables@immersivelabs.com>
2021-12-07 23:15:19 +0000

    day_5

--- day_5/day_5.exs
+++ day_5/day_5.exs
@@ -1,37 +1,41 @@
 defmodule Day5 do
   def part_one do
     coordinates = parse_input()
-    # coordinates = filter_diagonal(coordinates)
+    coordinates = filter_diagonal(coordinates)
+    result = draw_lines(coordinates)
+
+    count_two_or_more_overlaps(result)
+  end
+
+  def part_two do
+    coordinates = parse_input()
     result = draw_lines(coordinates)
 
     count_two_or_more_overlaps(result)
   end
 
   def draw_lines(coordinates) do
-    Enum.reduce(coordinates, %{}, fn([{x1,y1}, {x2,y2}], acc) ->
-      Enum.reduce(y1..y2, acc, fn y, acc ->
-        Enum.reduce(x1..x2, acc, fn x, acc ->
-          with {:ok, ys } <- Map.fetch(acc, y) do
-            with {:ok, xs} <- Map.fetch(ys, x) do
-              Map.put(acc, y, Map.put(ys, x, xs + 1))
-            else
-              _ ->
-                Map.put(acc, y, Map.put_new(ys, x, 1))
-            end
-          else
-            _ ->
-              Map.put_new(acc, y, %{x => 1})
-          end
-        end)
-      end)
+    Enum.flat_map(coordinates, fn([{x1,y1}, {x2,y2}]) ->
+      line_length = [abs(x1-x2), abs(y1-y2)] |> Enum.max
+      x_delta = delta(x1, x2)
+      y_delta = delta(y1, y2)
+
+      for x <- 0..line_length, do: { x1 + (x * x_delta), y1 + (x * y_delta) }
     end)
+  end
+
+  def delta(x1, x2) do
+    cond do
+      x1 < x2 -> 1
+      x1 == x2 -> 0
+      x1 > x2 -> -1
+    end
   end
 
   def count_two_or_more_overlaps(map) do
     map
-    |> Enum.reduce(0, fn {y, x}, acc ->
-      x |> Enum.reduce(acc, fn {x,count}, acc -> if count > 1, do: acc + 1, else: acc end)
-    end)
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    |> Enum.reduce(0, fn {_co, count}, acc -> if count > 1, do: acc + 1, else: acc end)
   end
 
   def parse_input do
@@ -48,6 +52,3 @@ defmodule Day5 do
     Enum.filter(coordinates, fn [{x1,y1}, {x2,y2}] -> x1 == x2 || y1 == y2 end)
   end
 end
-
-a = Day5.part_one()
-require IEx; IEx.pry
